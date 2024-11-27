0344b627c6e06508d38544aeabfa0a5191481da7
kgithub-source <kedar.tvd3@gmail.com>
2024-11-27 19:07:21 +0530

    unit testcases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,10 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(2,5)==7
+    assert add(8,4)==12
+
+def test_sub():
+    assert sub(3,1)==2
+    assert sub(7,2)==5
+    assert sib(1,2)==-1
